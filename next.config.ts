7939b215f5c7d4013e9b4c2011a5834c46a1ae10
argea-gh <argo.armail@gmail.com>
2026-02-23 14:15:17 +0700

    Disable ESLint during builds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,  // ✅ Disable ESLint error saat build
+  },
 };
 
 export default nextConfig;
